4f773a7d71e94b0b6ab981e6149df5c876be4cde
isaacg <iwanyoike65@gmail.com>
2023-01-30 18:04:38 +0300

    Check linter errors

--- script.js
+++ script.js
@@ -4,30 +4,28 @@ const sections = document.querySelectorAll('section');
 const menuContainer = document.querySelector('.menu-container');
 const mobileLinKItem = document.querySelectorAll('.mobile-link-item');
 
-ham.addEventListener('click',()=>{
-   
-   for (let section of sections) {
-      section.classList.add('blur');
-   }
-   
-   menuContainer.style.display = 'block';
-   
+ham.addEventListener('click', () => {
+  for (const section of sections) {
+    section.classList.add('blur');
+  }
+
+  menuContainer.style.display = 'block';
 });
 
-closeMenu.addEventListener('click', ()=>{
-   for (let section of sections) {
+closeMenu.addEventListener('click', () => {
+  for (const section of sections) {
+    section.classList.remove('blur');
+  }
+
+  menuContainer.style.display = 'none';
+});
+
+for (let i = 0; i < mobileLinKItem.length; i++) {
+  mobileLinKItem[i].addEventListener('click', () => {
+    for (const section of sections) {
       section.classList.remove('blur');
-   }
+    }
 
-   menuContainer.style.display = 'none';
-});
-
-for (var i = 0; i < mobileLinKItem.length; i++) {
-   mobileLinKItem[i].addEventListener('click', ()=>{
-      for (let section of sections) {
-         section.classList.remove('blur');
-      }
-   
-      menuContainer.style.display = 'none';
-   });
+    menuContainer.style.display = 'none';
+  });
 }
