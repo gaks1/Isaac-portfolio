0186f343bc3555594f6a91c4247b041f1f592160
chimake <dev.chimaobi@gmail.com>
2023-01-30 14:48:06 +0100

    Added DOM hide and display behaviour to mobile menu

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-const ham = document.querySelector('.nav-li5');
+const ham = document.querySelector('.hamburger');
+const closeMenu = document.querySelector('.close');
+const sections = document.querySelectorAll('section');
+const menuContainer = document.querySelector('.menu-container');
+const mobileLinKItem = document.querySelectorAll('.mobile-link-item');
 
 ham.addEventListener('click',()=>{
    
+   for (let section of sections) {
+      section.classList.add('blur');
+   }
+   
+   menuContainer.style.display = 'block';
    alert('works');
 });
+
+closeMenu.addEventListener('click', ()=>{
+   for (let section of sections) {
+      section.classList.remove('blur');
+   }
+
+   menuContainer.style.display = 'none';
+});
+
+for (var i = 0; i < mobileLinKItem.length; i++) {
+   mobileLinKItem[i].addEventListener('click', ()=>{
+      for (let section of sections) {
+         section.classList.remove('blur');
+      }
+   
+      menuContainer.style.display = 'none';
+   });
+}
