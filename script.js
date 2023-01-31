6b3e53f01dc3c9296cbeb355967554112e5a14f5
isaacg <iwanyoike65@gmail.com>
2023-01-31 11:50:20 +0300

    Reduce function logic in mobile menu hamburger javascript

--- script.js
+++ script.js
@@ -4,28 +4,25 @@ const sections = document.querySelectorAll('section');
 const menuContainer = document.querySelector('.menu-container');
 const mobileLinKItem = document.querySelectorAll('.mobile-link-item');
 
-ham.addEventListener('click', () => {
+function sectionFunc() {
   for (let i = 0; i < sections.length; i += 1) {
     sections[i].classList.remove('blur');
   }
 
   menuContainer.style.display = 'block';
-});
+}
 
-closeMenu.addEventListener('click', () => {
+function sectionHide() {
   for (let i = 0; i < sections.length; i += 1) {
     sections[i].classList.remove('blur');
   }
 
   menuContainer.style.display = 'none';
-});
+}
+ham.addEventListener('click', sectionFunc);
+
+closeMenu.addEventListener('click', sectionHide);
 
 for (let i = 0; i < mobileLinKItem.length; i += 1) {
-  mobileLinKItem[i].addEventListener('click', () => {
-    for (let i = 0; i < sections.length; i += 1) {
-      sections[i].classList.remove('blur');
-    }
-
-    menuContainer.style.display = 'none';
-  });
+  mobileLinKItem[i].addEventListener('click', sectionHide);
 }
